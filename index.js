89277c6deaf9a63c45a9eee22b3a4acdf0d6dca2
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-09-23 08:14:38 -0600

    commit tree c703386b9b1ea17eecd71cd82dc34ea73fd06ac2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer= 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'karen'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'gary';
+    return leastFavoriteCustomer;
+}
